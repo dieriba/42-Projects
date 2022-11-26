8e0f4589717e338d862a34062946752904b23d9f
Dieriba TOURE <dtoure@e2r7p4.clusters.42paris.fr>
2022-11-26 21:52:17 +0100

    pipex

--- test_project/test_pipex/test_here_doc.c
+++ test_project/test_pipex/test_here_doc.c
@@ -1,22 +1,52 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "get_next_line.h"
+#include <stdio.h>
 
-#define BUFFER_SIZE 10
+int ft_strncmp(char *s, char *s2, int len)
+{
+    size_t  i;
+    printf("s1 : %s", s);
+    printf("s2 : %s", s2);
+    i = -1;
+    while ((s[++i] && s2[i]) && (s[i] == s2[i]) && --len)
+        ;
+    if (!len)
+        return (1);
+    else
+        return (0);
+    printf("%d\n", s[i] - s2[i]);
+    return (s[i] - s2[i]);
+}
+
+size_t  ft_strlens(char *str)
+{
+    size_t  i;
+
+    i = -1;
+    while (str[++i])
+        ;
+    return (i);
+}
 
 int main (int argc, char **argv)
 {
     int fd;
-    char buffer[BUFFER_SIZE];
-    char buffer1[BUFFER_SIZE];
+    char *str;
+
+    if (argc != 3)
+        return (1);
     fd = open(argv[1], O_CREAT | O_TRUNC | O_WRONLY);
     if (fd < 0)
-        exit(1);
+        return (1);
     while (1)
     {
-        read(fd, buffer, BUFFER_SIZE);
-        write(fd, buffer, BUFFER_SIZE);
-        write(1, buffer, BUFFER_SIZE);
+        str = get_next_line(0);
+        if (ft_strncmp(str, argv[2], 3))
+            break;
+        write(fd, str, ft_strlens(str));
     }
+    
     close(fd);
 }
